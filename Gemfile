f045afc266826a90dba08370c17cf89d25ad59bd
cordelia <cordelia@example.com>
2022-05-24 10:33:38 +0900

    tweet devto article

--- Gemfile
+++ Gemfile
@@ -4,8 +4,7 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-gem "oauth"
 gem "json"
-gem "typhoeus"
+# gem "typhoeus"
 gem "twitter"
 gem "dotenv"
--- bot.rb
+++ bot.rb
@@ -12,13 +12,48 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
 
-# qiita api
-uri = URI('https://qiita.com/api/v2/items')
-res = Net::HTTP.get(uri)
+def getStrings(url)
+  begin
+    uri = URI(url)
+    responce = Net::HTTP.get(uri)
 
-index = rand(20)
-json = JSON.parse(res)
-title = json[index]["title"]
-url = json[index]["url"]
-hashTag = "#Qiita"
-client.update("#{title}\n#{url}\n#{hashTag}")
+    # Qiita
+    if url.include?("qiita")
+      index = rand(20)
+      json = JSON.parse(responce)
+      article = json[index]
+      title = article["title"]
+      url = article["url"]
+      tags = article["tags"]
+      hashTags = "#Qiita\n"
+
+      tags.each do |tag|
+        hashTags << "#" + tag["name"] + "\n"
+      end
+
+      return "#{title}\n#{url}\n#{hashTags}"
+
+    # Devto
+    elsif url.include?("dev.to")
+      index = rand(30)
+      json = JSON.parse(responce)
+      article = json[index]
+      title = article["title"]
+      url = article["url"]
+      tags = article["tags"].split(", ")
+      hashTags = "#DEVCommunity\n"
+
+      tags.each do |tag|
+        hashTags << "#" + tag + "\n"
+      end
+
+      return "#{title}\n#{url}\n#{hashTags}"
+    end
+
+  rescue
+    puts "Failed"
+  end
+end
+
+client.update(getStrings("https://qiita.com/api/v2/items"))
+client.update(getStrings("https://dev.to/api/articles/latest"))
